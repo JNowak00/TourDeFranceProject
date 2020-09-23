a2a42de376f902a2d929733c6ca314ca0559b680
Kub3X <59612682+Kub3X@users.noreply.github.com>
2020-09-23 15:09:29 +0200

    Add project files.

---
+++ TourDeFrance/Model/Cyclist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourDeFrance.Model
+{
+    class Cyclist
+    {
+        public string name { get; set; } 
+        public int enetID { get; set; } 
+        public string gender { get; set; } 
+        public string countryOrigin { get; set; }
+
+    }
+}
---
+++ TourDeFrance/ModelView/cyclistModelView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourDeFrance.Model;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
+using System.Diagnostics;
+using System.Windows;
+
+namespace TourDeFrance.ModelView
+{
+    class cyclistModelView
+    {
+        public void getCyclist()
+        {
+
+
+            List<Cyclist> cyclists = new List<Cyclist>();
+
+            XDocument xdoc = XDocument.Load("Cycling-Tour-De-France.xml");
+
+            IEnumerable<XElement> xes = from item in xdoc.Descendants("participant") select item;
+
+            foreach (var el in xes)
+            {
+
+                string name = el.Attribute("name").Value.ToString();
+                string gender = el.Attribute("gender").Value.ToString();
+                cyclists.Add(new Cyclist
+                {
+                    name = name,
+                    gender = gender
+
+                });
+
+            }
+            MessageBox.Show(cyclists[1].name);
+
+        }
+    }    
+}
+
